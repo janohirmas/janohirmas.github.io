5606ee10b0b3b1ceb51e262662a73151094e151a
abhirmas <janohirmas@gmail.com>
2020-05-03 11:56:37 +0200

    Make Elements appear when click(unfinished)

--- myfunctions.js
+++ myfunctions.js
@@ -1,4 +1,5 @@
 function DisplayContent(ID) {
+    // $("div").not(ID).style.display = "none";
     var x = document.getElementById(ID);
     if (x.style.display === "none") {
       x.style.display = "block";
