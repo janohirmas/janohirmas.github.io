7330090fbf4abd6828f336f9db697c8a9da694cf
abhirmas <janohirmas@gmail.com>
2020-05-13 12:30:58 +0200

    Initialize game page

--- myfunctions.js
+++ myfunctions.js
@@ -1,11 +1,13 @@
-function DisplayContent(ID) {
+function DisplayContent(Act) {
   HideEverything();
-  let x = document.getElementById(ID);
-  x.style.display = "block";
+  let x = document.getElementByClassName(Act);
+  for(let i = 0; i<x.length; i++) {
+    x[i].style.display = "block";
+  }
 }
 
 function HideEverything() {
-  let x = document.getElementsByClassName("content-box");
+  let x = document.getElementsByClassName("button-outcome");
   // console.log(x);
   for(let i = 0; i<x.length; i++) {
     x[i].style.display = "none";
