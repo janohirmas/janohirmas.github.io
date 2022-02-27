5781ab814d5db62e42ee9e41d122ec6513a3e871
janohirmas <janohirmas@gmail.com>
2022-02-27 13:26:36 +0100

    Added responsive design

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
+// CONSTANTS 
 const lNonEssential = [
     "GoogleAnalytics",
     "GoogleAnalytics2",
 ];
 
-function removeScripts() {
-    let lScripts = document.scripts;
-    if (localStorage.getItem('onlyEssentials')) {
-        for (let i=lScripts.length-1;i>=0; i+=-1) {
-            if (lNonEssential.includes(lScripts[i].id)) {
-                console.log(`Script ${lScripts[i].id} removed`);
-                lScripts[i].parentElement.removeChild(lScripts[i]);
-            };
-        };
-    };
-};
-// function to remove banner
-function removeBanner() {
-    let cookieContainer = document.getElementsByClassName('cookie-container')[0];
-    cookieContainer.classList.remove('active');
-    localStorage.setItem('cookieBannerDisplayed','true');
-}
-
+// INITIALIZE 
 document.addEventListener("DOMContentLoaded", function() {
     let cookieContainer = document.getElementsByClassName('cookie-container')[0];
     let AcceptButton = document.getElementById('cookies-all');
@@ -32,20 +16,77 @@ document.addEventListener("DOMContentLoaded", function() {
             cookieContainer.classList.add('active');
         }
     }, 2000);
+  
     // Remove Scripts if cookies already rejected
     removeScripts();
-    // Add Event listeners for buttons
-    AcceptButton.addEventListener('click', ()=> {
-        removeBanner();
-        localStorage.setItem('onlyEssentials','false');
-    });
+    mobileVersion((window.innerWidth<680));
     
-    RejectButton.addEventListener('click', ()=> {
-        removeBanner();
+});
+
+// Response to resize
+window.addEventListener('resize', ()=>{
+    mobileVersion((window.innerWidth<680));
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// FUNCTIONS
+
+function acceptCookies(bAll) {
+    if (bAll) {
+        localStorage.setItem('onlyEssentials','false');
+    } else {
         localStorage.setItem('onlyEssentials','true');
         removeScripts()
-    });    
+    }
+    removeBanner();
+}
+
+function mobileVersion(bMobile) {
+    btnHeader = document.getElementById('header-btn') ;
+    navHeader = document.getElementById('header-nav') ;
+    divFooter = document.getElementById('footer');
+
+    if (bMobile) {
+        btnHeader.classList.remove('hidden');
+        navHeader.classList.add('dropdown-content');  
+    } else {
+        btnHeader.classList.add('hidden');
+        navHeader.classList.remove('dropdown-content');
+    }
+}
+function removeScripts() {
+    let lScripts = document.scripts;
+    if (localStorage.getItem('onlyEssentials')) {
+        for (let i=lScripts.length-1;i>=0; i+=-1) {
+            if (lNonEssential.includes(lScripts[i].id)) {
+                console.log(`Script ${lScripts[i].id} removed`);
+                lScripts[i].parentElement.removeChild(lScripts[i]);
+            };
+        };
+    };
+};
+
+function removeBanner() {
+    let cookieContainer = document.getElementsByClassName('cookie-container')[0];
+    cookieContainer.classList.remove('active');
+    localStorage.setItem('cookieBannerDisplayed','true');
+}
 
 
-});
+function dropdownHeader() {
+    document.getElementById("header-nav").classList.toggle("show");
+}
 
